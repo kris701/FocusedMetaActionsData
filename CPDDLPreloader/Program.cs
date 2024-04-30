b6a7e0ebfc85fe448039ea8aae3fc81d7f8ea868
Kristian Skov Johansen <kris701kj@gmail.com>
2024-04-30 20:46:40 +0200

    Added premade CPDDL output files

---
+++ CPDDLPreloader/Program.cs
@@ -0,0 +1,38 @@
+﻿// Requires the CPDDL repository to be in bin
+
+using MetaActionGenerators;
+using PDDLSharp.ErrorListeners;
+using PDDLSharp.Models.PDDL.Domain;
+using PDDLSharp.Models.PDDL.Problem;
+using PDDLSharp.Parsers.PDDL;
+
+var benchmarks = "../../../../Benchmarks";
+var target = "../../../../CPDDLGroups";
+
+foreach(var benchmark in new DirectoryInfo(benchmarks).GetDirectories())
+{
+    var name = benchmark.Name;
+    Console.WriteLine($"Running for '{name}'");
+    var domainFile = new FileInfo(Path.Combine(benchmark.FullName, "domain.pddl")); 
+    var targetProblem = new FileInfo(Path.Combine(benchmark.FullName, "testing", "p15.pddl"));
+    var listener = new ErrorListener();
+    var parser = new PDDLParser(listener);
+    var domain = parser.ParseAs<DomainDecl>(domainFile);
+    var problem = parser.ParseAs<ProblemDecl>(targetProblem);
+    var argss = new Dictionary<string, string>()
+    {
+        { "cpddlExecutable", "cpddl/bin/pddl" },
+        { "tempFolder", "tmp" }
+    };
+    var generator = MetaGeneratorBuilder.GetGenerator(
+        MetaGeneratorBuilder.GeneratorOptions.CPDDLMutexed, 
+        domain, 
+        new List<ProblemDecl>() { problem },
+        argss);
+    generator.GenerateCandidates();
+
+    var outText = "<>";
+    if (File.Exists("tmp/output.txt"))
+        outText = File.ReadAllText("tmp/output.txt");
+    File.WriteAllText(Path.Combine(target, $"{name}.txt"), outText);
+} 
